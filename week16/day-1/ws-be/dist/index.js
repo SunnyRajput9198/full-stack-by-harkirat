640127f9d9069aaca17ab742dca9583634b4426a
sunny <rajputsny2@gmail.com>
2025-02-26 00:45:56 +0530

    added ws-fe

--- week16/day-1/ws-be/dist/index.js
+++ week16/day-1/ws-be/dist/index.js
@@ -25,9 +25,9 @@ const wss = new ws_1.WebSocketServer({ port: 8080 });
 // if hm  message me postman se ping bhejege to pong return krega
 wss.on("connection", function (socket) {
     console.log("hi there");
-    setInterval(() => {
-        socket.send("the cureent time is " + new Date().toLocaleTimeString());
-    }, 500);
+    // setInterval(()=>{
+    //     socket.send("the cureent time is "+new Date().toLocaleTimeString());
+    // },500);
     // if vha se kuch msg aaya
     socket.on("message", (e) => {
         if (e.toString() === "ping") {
@@ -36,3 +36,9 @@ wss.on("connection", function (socket) {
         ;
     });
 });
+// Notes:
+// 1. Open Postman.
+// 2. Choose the WebSocket option in Postman.
+// 3. Use the WebSocket URL format: ws://localhost:8000 (replace "8000" with the port you configured).
+// 4. Establish a connection from Postman to the WebSocket server.
+// 5. Once connected, the server will start sending random prices to Postman, and any message sent from Postman will be logged on the server.
--- week16/day-1/ws-be/src/index.ts
+++ week16/day-1/ws-be/src/index.ts
@@ -32,9 +32,9 @@ const wss = new WebSocketServer({ port: 8080 });
 // if hm  message me postman se ping bhejege to pong return krega
 wss.on("connection",function(socket){
     console.log("hi there");
-    setInterval(()=>{
-        socket.send("the cureent time is "+new Date().toLocaleTimeString());
-    },500);
+    // setInterval(()=>{
+    //     socket.send("the cureent time is "+new Date().toLocaleTimeString());
+    // },500);
 
     // if vha se kuch msg aaya
     socket.on("message",(e)=>{
@@ -45,3 +45,9 @@ wss.on("connection",function(socket){
 
 
 })
+// Notes:
+// 1. Open Postman.
+// 2. Choose the WebSocket option in Postman.
+// 3. Use the WebSocket URL format: ws://localhost:8000 (replace "8000" with the port you configured).
+// 4. Establish a connection from Postman to the WebSocket server.
+// 5. Once connected, the server will start sending random prices to Postman, and any message sent from Postman will be logged on the server.
